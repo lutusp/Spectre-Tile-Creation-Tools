c6d70ad3b1096bbc8acad9779d9863b268a04c38
Paul Lutus <lutusp@users.noreply.github.com>
2026-06-26 10:03:18 -0700

    Updated printable STL file script.
    Tuned the code to allow more tile shrinkage, for applications that need less tightly fitting parts.

--- spectre_STL_tile_generator.py
+++ spectre_STL_tile_generator.py
@@ -6,23 +6,39 @@ import cadquery as cq
 import math, os, sys
 
 # class arguments:
+
+# steps: how many discrete sections each edge wiggle has
+
+# tiles: how many graphic images and STL files to create
+
 # edge_length (mm): controls the overall
-# size of a tile, which derives from this length
-# amplitude: depth of edge waveform
-# shrink_factor shrinks the tile size
+# size of a tile, which is derived from this length
+
+# amplitude: depth of edge waveform, an excessive
+# value may prevent tiles from linking together
+
+# shrink_factor: shrinks the tile size
+# center-to-edge, units mm,
 # to allow interlocking of printed tiles 
-# without too much force
-# data_dir is a directory to receive
+# without too much force, an excessive
+# value may prevent creation of printable STL files
+
+# data_dir is an automatically created
+# local subdirectory to receive
 # graphic images and STL files 
 
 class PrintSpectre:
     def __init__(
         self,
+        steps = 32,
+        tiles = 8,
         edge_length=12,
         amplitude=1.0,
-        shrink_factor=0.075,
+        shrink_factor=0.25,
         data_dir="spectre_tiles",
     ):
+        self.steps = steps
+        self.tiles = tiles
         self.edge_length = edge_length
         self.amplitude = amplitude
         self.shrink_factor = shrink_factor
@@ -31,51 +47,7 @@ class PrintSpectre:
     # linear interpolation function
     def ntrp(self, x, xa, xb, ya, yb):
         return (x - xa) * (yb - ya) / (xb - xa) + ya
-
-    # a two-pass perimeter shrinking scheme
-    # to allow parts to fit snugly without jamming
-    # this function is called twice:
-    # cw, then ccw
-    def shrink_perimeter_core(self, xp, yp, sign=1):
-        xs = []
-        ys = []
-        for n in range(len(xp) - 1):
-            xa = xp[n]
-            xb = xp[n + 1]
-            ya = yp[n]
-            yb = yp[n + 1]
-            dx = xb - xa
-            dy = yb - ya
-            angle = math.atan2(dy, dx)
-            sx = -math.sin(angle) * self.shrink_factor * sign
-            sy = math.cos(angle) * self.shrink_factor * sign
-            xs += [-sx + xa]
-            ys += [-sy + ya]
-        return xs, ys
-
-    # call the perimeter shrink function twice
-    # then average the results
-    # sf is shrink factor, usually mm
-    def shrink_perimeter(self, xp, yp):
-        # resx = []
-        # resy = []
-        # Each call to the shrink_perimeter_core
-        # function trims the specified factor
-        # so apply 1/2 the specified factor, twice
-        # traverse clockwise
-
-        xs1, ys1 = self.shrink_perimeter_core(xp, yp, -1)
-        # xs2,ys2 = self.shrink_perimeter_core(xp, yp, -1)
-        # resx, resy = xs1, ys1
-        # traverse counterclockwise
-        xs2, ys2 = self.shrink_perimeter_core(xp[::-1], yp[::-1], 1)
-        # combine results
-        xs2.reverse()
-        ys2.reverse()
-        resx = [(a + b) / 2 for a, b in zip(xs1, xs2)]
-        resy = [(a + b) / 2 for a, b in zip(ys1, ys2)]
-        return resx, resy
-
+    
     # generate the canonical spectre tile
 
     def gen_spectre(self):
@@ -107,6 +79,51 @@ class PrintSpectre:
         yp += [0]
         return xp, yp
 
+    # a two-pass perimeter shrinking scheme
+    # to allow parts to fit snugly without jamming
+    # this function is called twice:
+    # cw, then ccw
+    def shrink_perimeter_core(self, xp, yp, sign=1):
+        if sign > 0:
+            xp = xp[::-1]
+            yp = yp[::-1]
+        xs = []
+        ys = []
+        for n in range(len(xp) - 1):
+            xa = xp[n]
+            xb = xp[n + 1]
+            ya = yp[n]
+            yb = yp[n + 1]
+            dx = xb - xa
+            dy = yb - ya
+            angle = math.atan2(dy, dx)
+            sx = -math.sin(angle) * self.shrink_factor * sign * 0.5
+            sy = math.cos(angle) * self.shrink_factor * sign * 0.5
+            xs += [-sx + xa]
+            ys += [-sy + ya]
+        if sign > 0:
+            xs = xs[::-1]
+            ys = ys[::-1]
+        return xs, ys
+
+    # we call the perimeter shrink function
+    # twice, once CW aound the tile, once CCW,
+    #  then average the results
+    def shrink_perimeter(self, xp, yp):
+        # resx = []
+        # resy = []
+        # Each call to the shrink_perimeter_core
+        # function trims the specified factor
+        # so apply 1/2 the specified factor, twice
+        # traverse clockwise
+        xs1, ys1 = self.shrink_perimeter_core(xp, yp, -1)
+        # traverse counterclockwise
+        xs2, ys2 = self.shrink_perimeter_core(xp,yp, 1)
+        # combine results
+        resx = [(a + b) * .5 for a, b in zip(xs1, xs2)]
+        resy = [(a + b) * .5 for a, b in zip(ys1, ys2)]
+        return resx,resy
+        
     # xs,ys, cartesian Spectre tile coordinates
     # amplitude 0 = classic spectre with straight edges
     # aplitude 1.0 = extreme spectre with deep wiggles
@@ -114,10 +131,6 @@ class PrintSpectre:
     def build_spectre(self, xs, ys, amplitude, plot=False):
         print(f"Building spectre with edge amplitude {amplitude:.02f} ...")
         sys.stdout.flush()
-        # avoid large "steps" values if possible
-        # this greatly increases the size
-        # of created STL files
-        steps = 32  # per spectre edge
         # internal amplitude conversion
         amp = amplitude * 0.3333
         xp = []
@@ -130,12 +143,12 @@ class PrintSpectre:
             # convert edge to polar coordinates
             angle = math.atan2((yb - ya), (xb - xa))
             radius = math.sqrt((yb - ya) ** 2 + (xb - xa) ** 2)
-            for n in range(steps):
+            for n in range(self.steps):
                 # create wiggle waveform using sine function
-                arg = self.ntrp(n, 0, steps, 0, math.pi * 2)
+                arg = self.ntrp(n, 0, self.steps, 0, math.pi * 2)
                 sw = math.sin(arg) * radius * amp
                 # mix orthogonal edge and sinewave components
-                rr = self.ntrp(n, 0, steps, 0, radius)
+                rr = self.ntrp(n, 0, self.steps, 0, radius)
                 xx = rr * math.cos(angle) + sw * math.sin(angle)
                 yy = rr * math.sin(angle) - sw * math.cos(angle)
                 xp += [xx + xa]
@@ -146,22 +159,22 @@ class PrintSpectre:
 
         # shrink the object's perimeter to avoid
         # an overly tight fit when printed and assembled
-        # essential for wiggly edges
+        # this is essential for wiggly edges
         xs, ys = self.shrink_perimeter(xp, yp)
-        # xs,ys = xp,yp
 
-        # create STL files for each form
+        # create STL files for each version
         # "thickness_mm" is the STL tile thickness in mm
         thickness_mm = 2
         points = [[x, y] for x, y in zip(xs, ys)]
 
-        # create cadquery object for spectre tile
-        spectre = cq.Workplane("XY").polyline(points).close().extrude(thickness_mm)
+        if True: # create STL files
+            # create cadquery object for spectre tile
+            spectre = cq.Workplane("XY").polyline(points).close().extrude(thickness_mm)
 
-        # save the spectre object in STL format
-        cq.exporters.export(
-            spectre, f"{self.data_dir}/spectre_tile_amplitude_{amplitude:.01f}.stl"
-        )
+            # save the spectre object in STL format
+            cq.exporters.export(
+                spectre, f"{self.data_dir}/spectre_tile_amplitude_{amplitude:.01f}.stl"
+            )
 
         if plot:
             # plot and save spectre tile images
@@ -177,7 +190,7 @@ class PrintSpectre:
                 xs,
                 ys,
                 color="#4080ff",
-                label=f"Shrunk {self.shrink_factor:0.3f} mm size",
+                label=f"Shrunk by {self.shrink_factor:0.3f} mm",
             )
 
             plt.legend(fontsize=16)
@@ -190,16 +203,25 @@ def main():
     data_dir = "spectre_tiles"
     if not os.path.isdir(data_dir):
         os.mkdir(data_dir)
-    edge_length = 12
-    amplitude = 1.0
-    shrink_factor = 0.075
+    # steps in the edge wiggle waveform 
+    steps = 32
+    # number of versions to create
+    tiles = 8
+    edge_length = 12 # mm per spectre edge
+    amplitude = 1 # depth of wiggles
+    # amount of shrinkage to allow
+    #  parts to easily fit together
+    shrink_factor = 0.25 # units mm
     # default spectre edge length mm
     edge_length = 12
-    spectre = PrintSpectre(edge_length, amplitude, shrink_factor, data_dir)
+    spectre = PrintSpectre(steps,tiles,edge_length, amplitude, shrink_factor, data_dir)
     # create the basic spectre data set
     xs, ys = spectre.gen_spectre()
-    tiles = 8
-    for n in range(tiles):
+    spectre.spx = xs
+    spectre.spy = ys
+    # tiles = number of tile versions moving 
+    # between flat sides and deep wiggles
+    for n in range(spectre.tiles):
         # apply edge wiggle amplitudes between 0 and 1
         amp = spectre.ntrp(n, 0, tiles - 1, 0, 1)
         spectre.build_spectre(xs, ys, amp, True)
